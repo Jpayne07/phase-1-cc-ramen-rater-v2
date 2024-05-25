9ce050b0398a4d3f1cb1e20f95980adeb084b46d
Jacob <jacobpayne007@gmail.com>
2024-05-25 00:04:10 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,97 @@
-// index.js
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  const images = document.querySelectorAll('.image');
+
+  images.forEach((obj, index) => {
+    obj.addEventListener('click', function() {
+      const ramenDetail = document.querySelector('.detail-image');
+      ramenDetail.setAttribute("src", obj.src)
+      console.log(ramen[0])
+      const foodName = document.querySelector("h2");
+      foodName.textContent = ramen[index].name;
+
+      const restaurant = document.querySelector(".restaurant");
+      restaurant.textContent = ramen[index].restaurant;
+
+      const rating = document.getElementById("rating-display");
+      rating.textContent = ramen[index].rating;
+
+      const comment = document.getElementById("comment-display");
+      comment.textContent = ramen[index].comment;
+    });
+  });
 };
 
 const addSubmitListener = () => {
-  // Add code
-}
+  const form = document.querySelector('form');
 
-const displayRamens = () => {
-  // Add code
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Code to handle form submission
+    let newRamenName = form.querySelector('input[name="name"]');
+    let newRamenRest = form.querySelector('input[name="restaurant"]');
+    let newRamenImg = form.querySelector('input[name="image"]');
+    let newRamenRting = form.querySelector('input[name="rating"]');
+    let newRamenComnt = form.querySelector('textarea[name="new-comment"]');
+     let newRamen = {
+      name:newRamenName.value,
+      restaurant:newRamenRest.value,
+      image:newRamenImg.value,
+      rating:newRamenRting.value,
+      comment:newRamenComnt.value
+     }
+
+     addRamen(newRamen)
+  });
 };
 
+
+function addRamen(ramen) {
+  fetch('http://localhost:3000/ramens/',{
+    method:"POST",
+     headers: {
+      "Content-Type": "application/json",
+      'Content-Type': 'application/x-www-form-urlencoded',
+  },
+
+  body: JSON.stringify(ramen),
+   
+
+  })
+.then(res => res.json())
+.then(ramen => console.log(data.length))
+}
+
+const displayRamens = (data) => {
+  document.addEventListener("DOMContentLoaded", (event) => {
+    fetch('http://localhost:3000/ramens')
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data here
+        data.forEach(obj => {
+          const ramenMenu = document.getElementById("ramen-menu")
+          const img = document.createElement("img")
+          img.setAttribute('src', obj.image);
+          img.setAttribute('class', "image")
+          ramenMenu.appendChild(img);
+          });
+        handleClick(data)
+
+      })
+      .catch(error => {
+        // Handle any errors that occur during the request
+        displayRamens
+      });
+  });
+
+  
+}
+
+
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  displayRamens()
+  addSubmitListener()
 }
 
 main()
@@ -27,3 +103,4 @@ export {
   handleClick,
   main,
 };
+
